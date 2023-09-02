6d0ab71b583f5d5a43416234fa1be06687193ff7
Ahmed Essam <alasad.elshaky@gmail.com>
2023-09-02 17:54:04 +0300

    Draft 1

---
+++ android/app/src/main/kotlin/com/example/bpapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bpapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/controller/network_controller.dart
@@ -0,0 +1,82 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'dart:io';
+
+class NetworkController extends GetxController {
+  final Connectivity _connectivity = Connectivity();
+
+  @override
+  void onInit() {
+    super.onInit();
+    _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  void _updateConnectionStatus(ConnectivityResult connectivityResult) {
+    Future<bool> connectivityChecker() async {
+      var connected = false;
+      print("Checking internet...");
+      try {
+        final result = await InternetAddress.lookup('google.com');
+        final result2 = await InternetAddress.lookup('facebook.com');
+        final result3 = await InternetAddress.lookup('microsoft.com');
+        if ((result.isNotEmpty && result[0].rawAddress.isNotEmpty) ||
+            (result2.isNotEmpty && result2[0].rawAddress.isNotEmpty) ||
+            (result3.isNotEmpty && result3[0].rawAddress.isNotEmpty)) {
+          print('connected..');
+          connected = true;
+          if (Get.isSnackbarOpen) {
+            Get.closeCurrentSnackbar();
+          }
+        } else {
+          print("not connected from else..");
+          connected = false;
+        }
+      } on SocketException catch (_) {
+        print('not connected...');
+        connected = false;
+      }
+      if (connected == false) {
+        Get.rawSnackbar(
+            messageText: const Text('PLEASE CONNECT TO THE INTERNET',
+                style: TextStyle(color: Colors.white, fontSize: 14)),
+            isDismissible: false,
+            duration: const Duration(days: 1),
+            backgroundColor: Colors.red[400]!,
+            icon: const Icon(
+              Icons.wifi_off,
+              color: Colors.white,
+              size: 35,
+            ),
+            margin: EdgeInsets.zero,
+            snackStyle: SnackStyle.GROUNDED);
+      } else if (connected == true) {
+        if (Get.isSnackbarOpen) {
+          Get.closeCurrentSnackbar();
+        }
+      }
+      return connected;
+    }
+
+    connectivityChecker();
+    // if (connectivityResult == ConnectivityResult.none) {
+    //   Get.rawSnackbar(
+    //       messageText: const Text('PLEASE CONNECT TO THE INTERNET',
+    //           style: TextStyle(color: Colors.white, fontSize: 14)),
+    //       isDismissible: false,
+    //       duration: const Duration(days: 1),
+    //       backgroundColor: Colors.red[400]!,
+    //       icon: const Icon(
+    //         Icons.wifi_off,
+    //         color: Colors.white,
+    //         size: 35,
+    //       ),
+    //       margin: EdgeInsets.zero,
+    //       snackStyle: SnackStyle.GROUNDED);
+    // } else {
+    //   if (Get.isSnackbarOpen) {
+    //     Get.closeCurrentSnackbar();
+    //   }
+    // }
+  }
+}
