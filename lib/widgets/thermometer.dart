ad14797b16da76e7800d8c2c9cd76ddbe4df9871
Ahmed Essam <alasad.elshaky@gmail.com>
2023-09-06 20:08:58 +0300

    Added Temperature Gauge

--- lib/widgets/thermometer.dart
+++ lib/widgets/thermometer.dart
@@ -8,47 +8,58 @@ class MyThermometer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SfLinearGauge(
-      minimum: 35,
-      maximum: 42,
-      interval: 1,
-      showTicks: true,
-      tickPosition: LinearElementPosition.cross,
-      majorTickStyle: const LinearTickStyle(
-        color: Colors.black87,
-        length: 20,
-        thickness: 2,
-      ),
-      minorTickStyle: const LinearTickStyle(
-        color: Colors.grey,
-        length: 15,
-        thickness: 1,
-      ),
-      minorTicksPerInterval: 9,
-      showLabels: true,
-      axisLabelStyle: const TextStyle(
-        color: Colors.black,
-        fontSize: 17.0,
-      ),
-      axisTrackStyle: const LinearAxisTrackStyle(
-        thickness: 24,
-        color: Color.fromRGBO(230, 207, 4, 1),
-        borderWidth: 2,
-        borderColor: Color.fromRGBO(202, 188, 56, 1),
-      ),
-      markerPointers: [
-        LinearShapePointer(
-          value: value,
-          shapeType: LinearShapePointerType.invertedTriangle,
-          color: Colors.blueGrey,
+    return Column(
+      children: [
+        const Text(
+          'Temperature',
+          style: TextStyle(
+            fontSize: 20,
+            color: Color.fromARGB(255, 52, 52, 52),
+          ),
         ),
-      ],
-      barPointers: [
-        LinearBarPointer(
-          value: value,
-          thickness: 10,
-          color: const Color.fromRGBO(118, 102, 88, 100),
-        )
+        SfLinearGauge(
+          minimum: 35,
+          maximum: 42,
+          interval: 1,
+          showTicks: true,
+          tickPosition: LinearElementPosition.cross,
+          majorTickStyle: const LinearTickStyle(
+            color: Colors.black87,
+            length: 20,
+            thickness: 2,
+          ),
+          minorTickStyle: const LinearTickStyle(
+            color: Colors.grey,
+            length: 15,
+            thickness: 1,
+          ),
+          minorTicksPerInterval: 9,
+          showLabels: true,
+          axisLabelStyle: const TextStyle(
+            color: Colors.black,
+            fontSize: 17.0,
+          ),
+          axisTrackStyle: const LinearAxisTrackStyle(
+            thickness: 24,
+            color: Color.fromRGBO(230, 207, 4, 1),
+            borderWidth: 2,
+            borderColor: Color.fromRGBO(202, 188, 56, 1),
+          ),
+          markerPointers: [
+            LinearShapePointer(
+              value: value,
+              shapeType: LinearShapePointerType.invertedTriangle,
+              color: Colors.blueGrey,
+            ),
+          ],
+          barPointers: [
+            LinearBarPointer(
+              value: value,
+              thickness: 10,
+              color: const Color.fromRGBO(118, 102, 88, 100),
+            )
+          ],
+        ),
       ],
     );
   }
