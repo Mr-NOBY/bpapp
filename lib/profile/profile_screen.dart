b4bfd0363cad0a2a98719d7321cd2ca8c23da735
Ahmed Essam <alasad.elshaky@gmail.com>
2024-01-21 22:54:58 +0200

    Updated profile screen

--- lib/profile/profile_screen.dart
+++ lib/profile/profile_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
+import '../controller/profile_controller.dart';
+import '../models/user_model.dart';
 import '../repositroy/auth_repository/auth_repo.dart';
 import 'update_profile_screen.dart';
 import 'widgets/profile_menu.dart';
@@ -11,6 +13,8 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(ProfileController());
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -32,70 +36,105 @@ class ProfileScreen extends StatelessWidget {
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              Stack(
-                children: [
-                  SizedBox(
-                    width: 120,
-                    height: 120,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: const Image(
-                        image: AssetImage("assets/images/user.png"),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                      width: 35,
-                      height: 35,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        color: Colors.grey[400],
-                      ),
-                      child: const Icon(
-                        LineAwesomeIcons.alternate_pencil,
-                        color: Colors.black,
-                        size: 20,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Full Name',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              Text(
-                'test@email.com',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: 200,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Get.to(() => UpdateProfileScreen());
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: BorderSide.none,
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                  ),
-                  child: const Text(
-                    'Edit Profile',
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
+              FutureBuilder(
+                  future: controller.getUserData(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        UserModel userData = snapshot.data as UserModel;
+
+                        final fullName = userData.fullName;
+                        final email = userData.email;
+
+                        return Column(
+                          children: [
+                            Stack(
+                              children: [
+                                SizedBox(
+                                  width: 120,
+                                  height: 120,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(100),
+                                    child: const Image(
+                                      image:
+                                          AssetImage("assets/images/user.png"),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: Container(
+                                    width: 35,
+                                    height: 35,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(100),
+                                      color: Colors.grey[400],
+                                    ),
+                                    child: const Icon(
+                                      LineAwesomeIcons.alternate_pencil,
+                                      color: Colors.black,
+                                      size: 20,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              fullName,
+                              style: Theme.of(context).textTheme.headlineSmall,
+                            ),
+                            Text(
+                              email,
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            SizedBox(
+                              width: 200,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Get.to(() => UpdateProfileScreen());
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  shape: const StadiumBorder(),
+                                  side: BorderSide.none,
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 15),
+                                ),
+                                child: const Text(
+                                  'Edit Profile',
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 30,
+                              width: double.infinity,
+                            ),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Center(
+                          child: Text(
+                            snapshot.error.toString(),
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        );
+                      } else {
+                        return const Center(
+                          child: Text("Something went wrong!"),
+                        );
+                      }
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  }),
               const Divider(),
               const SizedBox(
                 height: 10,
